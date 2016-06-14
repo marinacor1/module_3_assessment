dfc03ffd81c2128a799dae38eac7f21fbec84c7e
marinacor1 <marinaspam1234@gmail.com>
2016-06-14 09:07:43 -0600

    First controller test written for api

---
+++ test/controller/api/v1/items_controller_test.rb
@@ -0,0 +1,12 @@
+class Api::V1::ItemsControllerTest < ActionController < TestCase
+  test "Items#index" do
+    get :index, format: :json
+    parsed_json = JSON.parse(response.body)
+    assert_response :success
+    assert parsed_json.first.include?("Name")
+    assert parsed_json.first.include?("Description")
+    assert parsed_json.first.include?("Image Url")
+    refute parsed_json.first.include?("Created At")
+    refute parsed_json.first.include?("Updated At")
+  end
+end
