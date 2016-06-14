dc50c75b07ac44996d0426a992a33bdd323810d3
marinacor1 <marinaspam1234@gmail.com>
2016-06-14 09:17:28 -0600

    Controller test written

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -1,8 +1,12 @@
-class Api::V1::ItemsControllerTest < ActionController < TestCase
-  test "Items#index" do
+require 'test_helper'
+
+class Api::V1::ItemsControllerTest < ActionController::TestCase
+  test "should get index" do
     get :index, format: :json
     parsed_json = JSON.parse(response.body)
+
     assert_response :success
+
     assert parsed_json.first.include?("Name")
     assert parsed_json.first.include?("Description")
     assert parsed_json.first.include?("Image Url")
