7cda5a565dd619ecffa0b09c0950611bbfcebf7a
marinacor1 <marinaspam1234@gmail.com>
2016-06-14 09:30:54 -0600

    Route bug fixed with resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      get :items, only: [:index]
+      resources :items, only: [:index]
     end
   end
 end
