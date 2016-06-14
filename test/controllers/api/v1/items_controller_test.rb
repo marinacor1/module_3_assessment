2e8385823eb570ec300e33fd177a43b87355d392
marinacor1 <marinaspam1234@gmail.com>
2016-06-14 10:00:57 -0600

    Show test passing

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -7,9 +7,9 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
 
     assert_response :success
     assert_equal 2,  parsed_json.count
-    assert_equal "sock",  parsed_json.first['name']
-    assert_equal "its a sock",  parsed_json.first['description']
-    assert_equal "http://cdn3.volusion.com/uctgf.ukzte/v/vspfiles/photos/The-Carlton-2.jpg?1431093530", parsed_json.first['image_url']
+    assert_equal "sock",  parsed_json.last['name']
+    assert_equal "its a sock",  parsed_json.last['description']
+    assert_equal "http://cdn3.volusion.com/uctgf.ukzte/v/vspfiles/photos/The-Carlton-2.jpg?1431093530", parsed_json.last['image_url']
   end
 
   test "should get show" do
@@ -17,9 +17,9 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     parsed_json = JSON.parse(response.body)
 
     assert_response :success
-    assert_equal 2,  parsed_json.count
-    assert_equal "thing",  parsed_json.first['name']
-    assert_equal "its a thing",  parsed_json.first['description']
-    assert_equal "http://static.comicvine.com/uploads/original/11116/111163466/4650999-3128845011-300379.jpeg", parsed_json.first['image_url']
+    assert_equal Hash,  parsed_json.class
+    assert_equal "thing",  parsed_json['name']
+    assert_equal "its a thing",  parsed_json['description']
+    assert_equal "http://static.comicvine.com/uploads/original/11116/111163466/4650999-3128845011-300379.jpeg", parsed_json['image_url']
   end
 end
