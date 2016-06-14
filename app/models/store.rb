6150b277dd17c6b890622afc29869b11dd23bca2
marinacor1 <marinaspam1234@gmail.com>
2016-06-14 11:04:43 -0600

    Page outpus list of stores with correct information

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,12 +1,17 @@
 class Store < OpenStruct
   attr_reader :service
 
-  def initalize
-    @service = BuyService.new
+  def initialize
+    @service ||= BuyService.new
   end
 
   def store_information(zip)
-    stores = service.close_stores_hash(zip)
-    binding.pry
+    stores = service.close_stores_hash(zip)["stores"]
+    stores.map do |store|
+      {phone: store['phone'],
+      name: store['longName'],
+      distance: store['distance'],
+      type: store['storeType']}
+    end
   end
 end
